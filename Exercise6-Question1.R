17065d5152eb3cb81b3401f5d0a8ab21e5d89081
Yuting Zhang <yzhang49@nd.edu>
2019-11-02 22:33:57 -0400

    Q2

---
+++ Exercise6-Question1.R
@@ -0,0 +1,5 @@
+#Write R code that replicates the functionality of the head function we used in Unix. 
+#Your code should define a variable with the file to return lines from and a variable representing the number of lines to be returned from the top of the indicated file. 
+#The selected file content should be printed to the terminal in R.
+
+head(file,10)
