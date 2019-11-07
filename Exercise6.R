6881107fd77aea58985945fd1abebd5bf9a70a24
Yuting Zhang <yzhang49@nd.edu>
2019-11-07 16:25:19 -0500

    combination of Question1 and Question2

--- Exercise6.R
+++ Exercise6.R
@@ -1,3 +1,27 @@
+#Question1:
+#Write R code that replicates the functionality of the head function we used in Unix. 
+#Your code should define a variable with the file to return lines from and a variable representing the number of lines to be returned from the top of the indicated file. 
+#The selected file content should be printed to the terminal in R.
+
+#read in the file into a variable: 
+dat <- read.csv("iris.csv")
+
+
+#define a variable representing the number of lines to be returned:
+n=5
+
+
+#output the top n lines of the file: 
+head(dat,n)
+
+
+
+
+
+
+
+
+#Question2:
 #Load iris.csv into file
 file <- read.csv(file ="iris.csv")
   
